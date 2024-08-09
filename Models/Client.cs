671ab90f06d075300ac9f505304a2eb3e83ea0c4
jacksonUser <User@DESKTOP-U96UIA2>
2024-08-09 18:54:39 +0200

    commit after scaffolding

---
+++ Models/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CMPG323_PROJECT2_39990966.Models;
+
+public partial class Client
+{
+    public Guid ClientId { get; set; }
+
+    public string? ClientName { get; set; }
+
+    public string? PrimaryContactEmail { get; set; }
+
+    public DateTime? DateOnboarded { get; set; }
+}
---
+++ Models/ProductDescription.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CMPG323_PROJECT2_39990966.Models;
+
+public partial class ProductDescription
+{
+    public int ProductDescriptionId { get; set; }
+
+    public string Description { get; set; } = null!;
+
+    public Guid Rowguid { get; set; }
+
+    public DateTime ModifiedDate { get; set; }
+
+    public virtual ICollection<ProductModelProductDescription> ProductModelProductDescriptions { get; set; } = new List<ProductModelProductDescription>();
+}
