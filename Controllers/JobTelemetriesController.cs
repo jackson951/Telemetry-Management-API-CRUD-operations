4b438ce53016f0416365dfc45c5603bb9abadbbb
jacksonUser <User@DESKTOP-U96UIA2>
2024-08-10 23:56:12 +0200

    Final GetSavings methods

--- Controllers/JobTelemetriesController.cs
+++ Controllers/JobTelemetriesController.cs
@@ -178,14 +178,4 @@ namespace CMPG323_PROJECT2_39990966.Controllers
     }
 
 
-
-
-    // Define SavingsResult
-    public class SavingsResult
-    {
-        public int TotalHumanTime { get; set; }
-        public decimal TotalCostSaved { get; set; }
-    }
-
-
 }
